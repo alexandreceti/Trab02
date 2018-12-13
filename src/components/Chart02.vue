ac683a018a52f8310209fdd3f75e1e4056d6dc16
Alexandre C.C.Oliveira <alexandrecunha.eti@gmail.com>
2018-12-13 00:05:19 -0200

    graficos

---
+++ src/components/Chart02.vue
@@ -0,0 +1,29 @@
+<script>
+import { Doughnut } from 'vue-chartjs'
+
+export default {
+  extends: Doughnut,
+  mounted () {
+    // Overwriting base render method with actual data.
+    this.renderChart({
+      labels: ['O Poder das Conexões', 'O retorno do cangaceiro JavaScript', 'Kotlin com Android', 'Coleção Node.js', 'O universo da programação'],
+      datasets: [
+        {
+          backgroundColor: [
+            '#41B883',
+            '#E46651',
+            '#00D8FF',
+            '#DD1B16',
+            '#726291'
+          ],
+          data: [40, 20, 12, 39, 10]
+        }
+      ]
+    }, {responsive: true, maintainAspectRatio: false, legend: { display: true , position: "bottom" }})
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,17 +1,35 @@
 <template>
   <div class="container">
     <menu-nav></menu-nav>
-    <h1>Dashboard</h1>
-    <p> Dashboard</p>
+    <h1 class="is-size-1">Dashboard</h1>
+    <div class="columns">
+      <div class="column is-two-fifths">
+        <chart01 />
+      </div>
+      <div class="column">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis inventore optio maxime quod voluptatem magni! Facere obcaecati nam rerum totam assumenda maxime, vero voluptatem fugiat velit odit eveniet. Sunt, eaque!</p>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit ab accusamus ut totam perspiciatis? Veritatis ratione accusantium commodi ea optio porro eligendi reiciendis debitis illum nostrum natus repellat, rerum vero?</p>
+      </div>
+      <div class="column is-two-fifths">
+        <chart02 />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import menuNav from '../components/menuBar'
-
+import chart01 from '../components/Chart01'
+import chart02 from '../components/Chart02'
 export default {
   components: {
-    'menu-nav': menuNav
+    'menu-nav': menuNav,
+    'chart01': chart01,
+    'chart02': chart02
   },
   name: 'dashboard',
   props: {
